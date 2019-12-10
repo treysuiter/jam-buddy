1eb259a57244b664259673fe22b2a815d478437a
Trey Suiter <trey.suiter@gmail.com>
2019-12-10 14:17:35 -0600

    intrument drop down appearing but not functioning

--- src/components/setlist/SetlistList.js
+++ src/components/setlist/SetlistList.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import ApiManager from '../../modules/ApiManager'
 import SetlistCard from './SetlistCard'
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
+
+let defaultOption = ""
 
 function loggedInUserId() { return parseInt(localStorage.getItem("userId")) }
 
 export default class SetlistList extends Component {
 
   state = {
-    setlist: []
+    setlist: [],
+    instruments: []
   }
 
   componentDidMount() {
@@ -18,28 +23,37 @@ export default class SetlistList extends Component {
           setlist: setlistArray
         })
       })
+    ApiManager.getAll("instruments")
+      .then(instrumentArray => {
+        this.setState({
+          instruments: instrumentArray
+        })
+        defaultOption = this.state.instruments[0].instrumentName
+      })
   }
 
 
-render() {
+  render() {
 
-  return (
-    <>
-      <section className="section-content">
-      <button type="button" className="btn">Add Song</button>
-        <h1>Your Setlist</h1>
-      </section>
-      <div className="container-cards">
-        {this.state.setlist.map(songInSet =>
-          <SetlistCard
-            key={songInSet.id}
-            songTitle={songInSet.song.songTitle}
-            songInSet={songInSet}
-            {...this.props}
-          />
-        )}
-      </div>
-    </>
-  )
-}
+    return (
+      <>
+        There should be a dropdown here.
+      <Dropdown options={this.state.instruments.map(intName => intName.instrumentName)} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+        <section className="section-content">
+          <button type="button" className="btn">Add Song</button>
+          <h1>Your Setlist</h1>
+        </section>
+        <div className="container-cards">
+          {this.state.setlist.map(songInSet =>
+            <SetlistCard
+              key={songInSet.id}
+              songTitle={songInSet.song.songTitle}
+              songInSet={songInSet}
+              {...this.props}
+            />
+          )}
+        </div>
+      </>
+    )
+  }
 }
