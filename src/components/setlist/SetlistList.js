e5ed7d1f781a2015f0626e985aec8cf3e8660026
Trey Suiter <trey.suiter@gmail.com>
2019-12-10 12:20:05 -0600

    setlists are viewable per logged in user

--- src/components/setlist/SetlistList.js
+++ src/components/setlist/SetlistList.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
+import ApiManager from '../../modules/ApiManager'
+import SetlistCard from './SetlistCard'
+
+function loggedInUserId() { return parseInt(localStorage.getItem("userId")) }
 
 export default class SetlistList extends Component {
 
-  render () {
-    return (
-      <>
-      This is the Setlist page.
-      </>
-    )
+  state = {
+    setlist: []
   }
+
+  componentDidMount() {
+
+    ApiManager.getAll("setlists", `userId=${loggedInUserId()}&_expand=song`)
+      .then(setlistArray => {
+        this.setState({
+          setlist: setlistArray
+        })
+      })
+  }
+
+
+render() {
+
+  return (
+    <>
+      <section className="section-content">
+        <h1>Your Setlist</h1>
+      </section>
+      <div className="container-cards">
+        {this.state.setlist.map(songInSet =>
+          <SetlistCard
+            key={songInSet.id}
+            songTitle={songInSet.song.songTitle}
+            songInSet={songInSet}
+            {...this.props}
+          />
+        )}
+      </div>
+    </>
+  )
+}
 }
