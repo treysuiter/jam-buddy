c3bb903f7e80a4084a2d5c2a6c4d843991e4af18
Trey Suiter <trey.suiter@gmail.com>
2019-12-10 19:13:53 -0600

    remove functions from CustomerApiManager

--- src/modules/CustomApiManager.js
+++ src/modules/CustomApiManager.js
@@ -1,14 +1,1 @@
-import ApiManager from "./ApiManager"
 
-export default {
-
-addNewSongToDatbase() {
-  const song = {
-    songTitle: this.state.songTitle,
-    artistName: this.state.artistName,
-    deezerId: ""
-  }
-  ApiManager.post("songs", song)
-}
-
-}
