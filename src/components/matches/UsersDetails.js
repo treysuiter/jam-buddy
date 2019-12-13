9b2d79d8144edcf0c599b57bae75b38b4ee1af96
Trey Suiter <trey.suiter@gmail.com>
2019-12-13 15:55:18 -0600

    doing stuff

--- src/components/matches/UsersDetails.js
+++ src/components/matches/UsersDetails.js
@@ -1,38 +1,44 @@
 import React, { Component } from 'react';
 import ApiManager from '../../modules/ApiManager';
+import SetlistCard from './SetlistCard';
 
 
 export default class UsersDetail extends Component {
 
   state = {
-      name: "",
-      instrument: "",
-      setlist: "",
-      loadingStatus: true
-  } 
+    name: "",
+    instrument: "",
+    setlist: [],
+    loadingStatus: true
+  }
 
-  componentDidMount(){
+  componentDidMount() {
 
     //http://localhost:5002/users/1?_embed=setlists&_expand=instruments
     //get(id) from AnimalManager and hang on to the data; put it into state
-    ApiManager.get("users", this.props.matchId, "_embed=setlists&_expand=instrument" )
-    .then((user) => {
-      console.log(user)
-      this.setState({
-        name: user.name,
-        instrument: user.instrument.instrumentName,
-        setlist: "user.setlist",
-        loadingStatus: false
+    ApiManager.get("users", this.props.matchId, "_embed=setlists&_expand=instrument")
+      .then((user) => {
+        this.setState({
+          name: user.name,
+          instrument: user.instrument.instrumentName,
+          setlist: user.setlist,
+          loadingStatus: false
+        });
       });
-    });
+    //http://localhost:5002/setlists?userId=1&_expand=song
+    ApiManager.getAll("setlists", `userId=${this.props.matchId}&_expand=song`)
+      .then(response => {
+        this.setState({
+          setlist: response,
+        })
+      })
   }
-
-//   handleDelete = () => {
-//     //invoke the delete function in AnimalManger and re-direct to the animal list.
-//     this.setState({loadingStatus: true})
-//     APIManager.delete("animals", this.props.animalId)
-//     .then(() => this.props.history.push("/animals"))
-// }
+  //   handleDelete = () => {
+  //     //invoke the delete function in AnimalManger and re-direct to the animal list.
+  //     this.setState({loadingStatus: true})
+  //     APIManager.delete("animals", this.props.animalId)
+  //     .then(() => this.props.history.push("/animals"))
+  // }
 
   render() {
     return (
@@ -41,10 +47,19 @@ export default class UsersDetail extends Component {
           <picture>
             <img src={`https://robohash.org/${this.state.name}`} alt="Current User" />
           </picture>
-            <h3>Name: {this.state.name}</h3>
-            <p>Instrument: {this.state.instrument}</p>
-            <button type="button" disabled={this.state.loadingStatus}>Add Buddy</button>
-            <button type="button" disabled={this.state.loadingStatus}>Remove Buddy</button>
+          <h3>Name: {this.state.name}</h3>
+          <p>Instrument: {this.state.instrument}</p>
+          <div className="userSetlist">
+            {this.state.setlist.map(setlistSong =>
+              <SetlistCard
+                key={setlistSong.id}
+                setlistSong={setlistSong}
+                songName={setlistSong.song.songTitle}
+                {...this.props}
+              />)}
+          </div>
+          <button type="button" disabled={this.state.loadingStatus}>Add Buddy</button>
+          <button type="button" disabled={this.state.loadingStatus}>Remove Buddy</button>
         </div>
       </div>
     );
