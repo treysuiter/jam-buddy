1d4c684f4252d162edbaeae3d05d8292d7f39cfe
Trey Suiter <trey.suiter@gmail.com>
2019-12-13 12:20:14 -0600

    editing formatting for setlist card

--- src/components/matches/MatchesCard.js
+++ src/components/matches/MatchesCard.js
@@ -8,7 +8,7 @@ export default class MatchesCard extends Component {
       (
         <div className="card">
           <h3>{this.props.matchName}</h3>
-          <h4>{this.props.songMatchTotal}</h4>
+          <h4>Setlist Matches: {this.props.songMatchTotal}</h4>
           <button type="button" className="btn" onClick={() => this.props.seeDetails(this.props.match.id)}>Details</button>
         </div>
       )
