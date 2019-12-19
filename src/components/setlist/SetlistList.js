035624cb2d190da5a5914f9b557a901e685db014
Trey Suiter <trey.suiter@gmail.com>
2019-12-19 13:01:20 -0600

    songs getting added to song db twice fixed with checking for song on song db and setlist db prior

--- src/components/setlist/SetlistList.js
+++ src/components/setlist/SetlistList.js
@@ -132,7 +132,6 @@ class SetlistList extends Component {
   checkForSongInDatabase = (deezerId) => {
     return ApiManager.getAll("songs", `deezerId=${deezerId}`)
       .then(response => {
-        console.log(deezerId, "dezer Id", this.state, "this sate", response, response.length > 0, "what is this response for check for song in db?")
         if (response.length > 0) {
           return true
         } else {
@@ -196,59 +195,46 @@ class SetlistList extends Component {
 
       ApiManager.deezer(this.state.artistName, this.state.songTitle)
         .then(deezerResponse => {
-          this.setState({
-            artistName: deezerResponse.data[0].artist.name,
-            songTitle: deezerResponse.data[0].title,
-            deezerId: deezerResponse.data[0].id
-          })
-        })
-        .then(() => {
-
-          this.checkForSongInDatabase(this.state.deezerId)
-            .then(bool => {
-              console.log(bool, "check  for song in db bool")
-              if (!bool) {
-                console.log("song IS NOT in db")
-                ApiManager.deezer(this.state.artistName, this.state.songTitle)
-                  .then(deezerResponse => {
-                    if (deezerResponse.data.length > 0) {
-                      const song = {
-                        songTitle: this.state.songTitle,
-                        artistName: this.state.artistName,
-                        deezerId: this.state.deezerId
-                      }
-                      ApiManager.post("songs", song)
-                        .then(response => {
-                          const newSetlistSong = {
-                            songId: response.id,
-                            userId: loggedInUserId()
-                          }
-                          ApiManager.post("setlists", newSetlistSong)
-                            .then(() => {
-                              this.setlistRerender()
-                            })
-                        })
-
-                    } else {
-
-                      window.alert("Song not found. Please try search again.")
-                    }
-                  })
-
-              } else {
-                console.log("song IS in db")
-                this.addSongToSetlist(this.state.deezerId)
-              }
+          console.log(deezerResponse, "deezer response")
+          if (deezerResponse.data.length === 0) {
+            window.alert("Song not found. Please try search again.")
+          } else {
+            this.setState({
+              artistName: deezerResponse.data[0].artist.name,
+              songTitle: deezerResponse.data[0].title,
+              deezerId: deezerResponse.data[0].id
             })
-        })
-    }
-    this.setState({
-      artistName: "",
-      songTitle: "",
-      deezerId: ""
-    })
-    evt.target.reset()
+
+            this.checkForSongInDatabase(this.state.deezerId)
+              .then(bool => {
+                if (!bool) {
+                  const song = {
+                    songTitle: this.state.songTitle,
+                    artistName: this.state.artistName,
+                    deezerId: this.state.deezerId
+                  }
+                  ApiManager.post("songs", song)
+                    .then(response => {
+                      const newSetlistSong = {
+                        songId: response.id,
+                        userId: loggedInUserId()
+                      }
+                      ApiManager.post("setlists", newSetlistSong)
+                        .then(() => {
+                          this.setlistRerender()
+                        })
+                    })
+                } else {
+                  this.addSongToSetlist(this.state.deezerId)
+                }
+              })
+          }
+        }
+        )
+    } evt.target.reset()
   }
+
+  // evt.target.reset()
 
   render() {
 
