291f7df40b0120d1e84416a5800789787df40cc2
Trey Suiter <trey.suiter@gmail.com>
2019-12-30 14:16:44 -0600

    matchescard now display buddy in italics if user is your buddy

--- src/components/matches/MatchesCard.js
+++ src/components/matches/MatchesCard.js
@@ -5,6 +5,9 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import ApiManager from '../../modules/ApiManager';
+
+function loggedInUserId() { return parseInt(localStorage.getItem("userId")) }
 
 const styles = {
   matchesCard: {
@@ -33,6 +36,19 @@ const styles = {
 
 class MatchesCard extends Component {
 
+  state = {
+    isThisMyBuddy: false
+  }
+
+  componentDidMount() {
+    ApiManager.getAll("buddies", `userId=${this.props.matchObj.id}&loggedInUser=${loggedInUserId()}`)
+    .then(response => {
+      this.setState({
+        isThisMyBuddy: response.length > 0 ? true : false
+      })
+    })
+  }
+
   render() {
 
     const { classes } = this.props;
@@ -41,7 +57,7 @@ class MatchesCard extends Component {
       <Card className={classes.matchesCard}>
         <CardContent>
           <Typography className={classes.title} color="textPrimary" gutterBottom>
-            {this.props.matchName}
+            {this.state.isThisMyBuddy ? <>{this.props.matchName} <i>(buddy)</i></> : this.props.matchName}
           </Typography>
           <Typography label="Match Total:" className={classes.artist} color="textSecondary">
             {`Match Total: ${this.props.songMatchTotal}`}
